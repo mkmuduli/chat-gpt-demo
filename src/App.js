c380c1b0777cff9dd5504482a6d9adac5f6dd4db
Manoj <apple@mk-personal.local>
2023-04-30 20:33:18 +0530

    Image search done

--- src/App.js
+++ src/App.js
@@ -2,31 +2,35 @@ import './App.css';
 import './comp/header.css';
 import './comp/imageList.css';
 import './comp/switch.css'
-import Header  from './comp/Header';
+import Header from './comp/Header';
 import { useState } from 'react';
 import { fetchImagesFromDALLE } from './utils/imageHelper';
 import ImageList from './comp/ImageList';
+import Suggestion from './comp/SuggetionList';
 
 function App() {
 
-  const [type,setType] = useState("image");
-  const [images,setImages] = useState([]);
+  const [type, setType] = useState("image");
+  const [images, setImages] = useState([]);
 
-  const handleTypeChange = (val) =>{
+  const handleTypeChange = (val) => {
     setType(val)
   }
 
-  const handleFetch = (bodyData)=>{
-    if(type === 'image'){
+  const handleFetch = (bodyData) => {
+    if (type === 'image') {
       const chatGPTAPIKEY = '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';
-      fetchImagesFromDALLE(bodyData.prompt,chatGPTAPIKEY,4).then(setImages)
+      fetchImagesFromDALLE(bodyData.prompt, chatGPTAPIKEY, 4).then(setImages)
     }
   }
 
   return (
     <main className="main-container">
       <Header type={type} onTypeChange={handleTypeChange} onFetch={handleFetch} />
-      <ImageList images={images} />
+      {type === "image" ?
+        <ImageList images={images} />
+        :
+        <Suggestion />}
     </main>
   );
 }
